0234736421201650d4a3da6df4653e80951b1205
Lucas Castro <lucas.castro@gmail.com>
2022-08-15 22:32:14 -0400

    feat: Added sample usage of `style` in Next.js sample.

--- examples/nextjs-sample/pages/sample.tsx
+++ examples/nextjs-sample/pages/sample.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import type { NextPage } from "next"
-import { Box, Column, Heading, Text, Anchor, appDarkTheme, appTheme, exampleClass } from "@/ui"
+import { Box, Column, Heading, Text, Anchor, appDarkTheme, appTheme, keyframes, useColors, getTheme, style } from "@/ui"
 import { Cards } from "@/components/sample/Cards"
 import { Tags } from "@/components/sample/Tags"
 import { ButtonSamples } from "@/components/sample/ButtonSamples"
@@ -15,26 +15,106 @@ const Sample: NextPage = () => {
   const showInputs = true
   const showTags = true
   const showStatuses = true
-  const showSampleBox = false
+  const showSampleBox = true
+
+  const nuiProps = style(
+    {
+      bg: "$neutral10",
+      color: "$textNeutral10",
+      mt: "$8",
+      mb: "$20",
+      ml: "$16",
+      mr: "$12",
+      pl: "$16",
+      pr: "$32",
+      py: "$32",
+      radiusTopLeft: "$2",
+      radiusTopRight: "$8",
+      radiusBottomLeft: "$6",
+      radiusBottomRight: "$3",
+      float: "left",
+      maxWidth: "$480",
+      fontWeight: "$600",
+      fontSize: "$h3",
+      // animation: "$spin",
+      // animation: "$spinAndPause",
+      // animation: "$flashSize",
+      // animation: "$bounceTop",
+      // animation: "$bounceRight",
+      // animation: "$bounceBottom",
+      // animation: "$bounceLeft",
+      // animation: "$slideInTop",
+      // animation: "$slideOutTop",
+      // animation: "$slideInRight",
+      // animation: "$slideOutRight",
+      // animation: "$slideInBottom",
+      // animation: "$slideOutBottom",
+      // animation: "$slideInLeft",
+      // animation: "$slideOutLeft",
+      // animation: "$fadeIn",
+      // animation: "$fadeOut",
+      // animation: "$zoomIn",
+      // animation: "$zoomOut",
+      // ":active": {
+      //   color: "$textPrimary10",
+      //   bg: "$primary10",
+      // },
+      // ":focus-visible": {
+      //   color: "$textNeutral10",
+      //   bg: "$warningMax",
+      // },
+      // ":hover": {
+      //   bg: "$secondary9",
+      // },
+      "@dark": {
+        outlineWidth: "$widthBase",
+      },
+      "@reducedMotion": {
+        animation: "none",
+      },
+    },
+    {
+      "@sm": false,
+      "@md": false,
+      "@lg": false,
+      "@xl": false,
+      "!sm": false,
+      "!md": false,
+      "!lg": false,
+      "!xl": false,
+      "@highContrast": false,
+      "@reducedMotion": false,
+      "@reducedData": false,
+      "@touch": false,
+      "@pointer": false,
+      "@tv": false,
+      "!highContrast": false,
+      "!reducedMotion": false,
+      "!reducedData": false,
+      "!touch": false,
+      "!pointer": false,
+      "!tv": false,
+      "@light": false,
+      "@dark": false,
+      debug: true,
+    },
+    "SampleBox"
+  )
+  console.log("nuiProps", nuiProps)
 
   const sampleControls = (
     <>
-      {showColorPalette && (
-        <Cards title="Color Palette" theme={appTheme} darkTheme={appDarkTheme} />
-      )}
+      {showColorPalette && <Cards title="Color Palette" theme={appTheme} darkTheme={appDarkTheme} />}
       <Box mt="6" maxWidth="25" mx="auto">
         {showTypography && (
           <>
             <Heading>
-              <Anchor href="#">
-                &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit&quot;
-              </Anchor>
+              <Anchor href="#">&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit&quot;</Anchor>
             </Heading>
             <Text.p>
-              Lorem &apos;ipsum&apos; dolor sit amet -- consectetur adipiscing elit. Donec
-              scelerisque quis est non pharetra. &quot;Etiam&quot; at lacus arcu. Nullam vitae{" "}
-              <Anchor href="#">varius nisl semper</Anchor> nulla ac ipsum ultricies hendrerit sit
-              amet metus.
+              Lorem &apos;ipsum&apos; dolor sit amet -- consectetur adipiscing elit. Donec scelerisque quis est non
+              pharetra. &quot;Etiam&quot; at lacus arcu. Nullam vitae <Anchor href="#">varius nisl semper</Anchor> nulla
+              ac ipsum ultricies hendrerit sit amet metus.
             </Text.p>
           </>
         )}
@@ -53,20 +133,22 @@ const Sample: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {showSampleBox && (
-        <Box
-          className={`debug ${exampleClass}`}
-          css={{
-            bg: "$neutral10",
-            color: "$textNeutral10",
-            margin: "$2 $4 $2 $6",
-            p: "$8 $8 $8 $4",
-            borderRadius: "$2 $5 $1 $4",
-            float: "left",
-            maxWidth: "$20",
-          }}
-        >
-          Sample box
-        </Box>
+        <>
+          <Box
+            css={{
+              bg: "$neutral10",
+              color: "$textNeutral10",
+              margin: "$2 $4 $7 $6",
+              p: "$8 $8 $8 $4",
+              borderRadius: "$2 $5 $1 $4",
+              float: "left",
+              maxWidth: "$20",
+            }}
+          >
+            Sample box
+          </Box>
+          <div {...nuiProps}>NUI-powered sample box</div>
+        </>
       )}
       {!showSampleBox && sampleControls}
     </Column.article>
