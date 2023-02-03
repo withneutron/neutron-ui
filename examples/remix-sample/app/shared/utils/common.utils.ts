acf92b113c85200a186cf81803387defb4587283
Lucas Castro <lucas.castro@gmail.com>
2023-02-02 19:31:50 -0500

    chore: Version clean-up on packages; removed some stale example apps.

--- examples/remix-sample/app/shared/utils/common.utils.ts
+++
@@ -1,4 +0,0 @@
-export const toKebabCase = (text: string): string => {
-  const matches = text.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-  return matches && matches.length > 0 ? matches.map(x => x.toLowerCase()).join("-") : ""
-}
--- examples/remix-sample/app/shared/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./common.utils"
-export * from "./store.utils"
