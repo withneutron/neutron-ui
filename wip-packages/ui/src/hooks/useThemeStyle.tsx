1e573c3411e8be95af3ba5e82a8ced9965eb3caf
Lucas Castro <lucas.castro@gmail.com>
2024-05-15 11:33:53 -0400

    chore: Moved unfinished `ui` package out of the build path.

--- wip-packages/ui/src/hooks/useThemeStyle.tsx
+++ wip-packages/ui/src/hooks/useThemeStyle.tsx
@@ -1,11 +1,11 @@
 import { useMemo } from "react"
-import { getTheme } from "@withneutron/quarks"
+import { getThemeOverrides } from "@withneutron/quarks"
 import { useColors } from "./useColors"
 
 export function useThemeStyle() {
   const { isDark } = useColors()
 
-  const style = useMemo(() => getTheme(isDark ? "dark" : "light"), [isDark])
+  const style = useMemo(() => getThemeOverrides(isDark ? "dark" : "light"), [isDark])
   const stringified = useMemo(
     () =>
       Object.entries(style)
